fd3038a455d6f01b753743727f3da989d1d1424d
gawbsouza <gabriel.souza@zoho.com>
2024-03-27 03:00:24 +0100

    ex02: fix unused parse

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Base.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gasouza <gasouza@student.42sp.org.br >     +#+  +:+       +#+        */
+/*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 13:05:32 by gasouza           #+#    #+#             */
-/*   Updated: 2024/03/17 13:38:37 by gasouza          ###   ########.fr       */
+/*   Updated: 2024/03/27 02:59:12 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,19 +50,19 @@ void Base::identify( Base * p )
 void Base::identity( Base & p )
 {
     try {
-        dynamic_cast<A&>(p);
+        A a = dynamic_cast<A&>(p);
         std::cout << "Class A" << std::endl;
         return;
     } catch(std::exception &e) {}
 
     try {
-        dynamic_cast<B&>(p);
+        B b = dynamic_cast<B&>(p);
         std::cout << "Class B" << std::endl;
         return;
     } catch(std::exception &e) {}
 
     try {
-        dynamic_cast<C&>(p);
+        C c = dynamic_cast<C&>(p);
         std::cout << "Class C" << std::endl;
         return;
     } catch(std::exception &e) {}
